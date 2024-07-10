fb82f1d41ac1d9b9d070a4a723ef2bd946947a95
Yasakatsu <chappieoct4@gmail.com>
2024-07-11 08:47:36 +0900

    背景画像の修正

--- bulls-cut/resources/views/sections/concept.blade.php
+++ bulls-cut/resources/views/sections/concept.blade.php
@@ -1,9 +1,9 @@
-<section id="concept" class="py-8 px-5 sm:text-xl bg-center md:bg-right object-cover bg-no-repeat"
+<section id="concept" class="bg-cover sm:bg-contain sm:bg-right bg-no-repeat py-8 px-5 sm:text-xl "
     style="background-image: url(' {{ asset('/images/S__10690572_0.jpg') }}');">
-    <h2 id="concept-title" class="text-3xl pb-8  font-bold font-sans">
+    <h2 id="concept-title" class="text-3xl pb-8 font-bold font-sans">
         Concept
     </h2>
-    <div id="concept-main" class=" text-bold tracking-widest leading-10 ">
+    <div id="concept-main" class="bg-stone-900 sm:bg-none bg-opacity-40 sm:bg-opacity-0 rounded-lg px-4 py-4 text-bold leading-10  ">
         <p>完全個室のメンズ専門理容室。<br>
             ゆったりと落ち着ける空間には<br>
             フルフラットチェアをご用意。<br>
