be60cde493e706cfdcddaa13330aabc6f2f8278c
tsukasa <chappieoct4@gmail.com>
2024-08-05 18:27:26 +0900

    スタッシュ

---
+++ barbarshop/resources/views/sections/hours.blade.php
@@ -0,0 +1,16 @@
+        <section id="hours" class=" sm:text-xl text-center py-8 px-5"
+            style="background-image: url('{{ asset('/images/BULLSCUT_logotype.png') }}'); background-size: 50%; background-repeat: no-repeat; background-position: left 30px top 40px">
+            <div>
+                <h2 class="text-3xl pb-8 font-bold font-sans">Opening Hours</h2>
+                <div>
+                    <div class="pb-4">
+                        <p class="font-semibold">Monday - Friday</p>
+                        <p class="">12:00 - 23:00 (Last Order at 22:00 )</p>
+                    </div>
+                    <div>
+                        <p class="font-semibold">Weekends and Holidays </p>
+                        <p>10:00 - 21:00 (Last Order at 20:00 )</p>
+                    </div>
+                </div>
+            </div>
+        </section>
