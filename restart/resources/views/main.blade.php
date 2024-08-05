5044ae9e9a80602dc33ad97ba486d7356489ebe4
tsukasa <chappieoct4@gmail.com>
2024-08-06 08:41:12 +0900

    resourcesディレクトリ差し替え

--- restart/resources/views/main.blade.php
+++ restart/resources/views/main.blade.php
@@ -11,9 +11,9 @@
         @include('sections.concept')
         @include('sections.service')
         @include('sections.menu')
+        @include('sections.reserve')
         @include('sections.hours')
         @include('sections.address')
-        @include('sections.reserve')
         @include('sections.social')
     </main>
     @include('partials.footer')
---
+++ restart/resources/views/sections/address.blade.php
@@ -0,0 +1,20 @@
+        <section id="address" class="text-center stext-lg sm:text-xl py-4">
+            <div
+                style="background-image: url('{{ asset('/images/BULLSCUT_logotype.png') }}'); background-size: 50%; background-repeat: no-repeat; background-position:center ">
+
+                <h2 class="text-3xl pb-4 font-bold font-sans">Adress
+                </h2>
+                <address class="pb-1 font-sans">
+                    〒 134 - 0083<br>
+                    東京都 江戸川区 中葛西 2-27-11 サニーレジデンス1F<br>
+                    TEL: 03 - 6808 -0838
+                </address>
+            </div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3241.837877080688!2d139.8695653152589!3d35.65858498019867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188b5856e912bb%3A0x7f3281cba6201b2e!2s2-ch%C5%8Dme-27-11%20Nakakasai%2C%20Edogawa%20City%2C%20Tokyo%20134-0083%2C%20Japan!5e0!3m2!1sja!2sjp&hl=ja"
+                style="border:0;" allowfullscreen="" loading="lazy" class="block mx-auto w-8/12 h-80"></iframe>
+            <a class="text-xs md:text-lg underline hover:text-violet-500"
+                href="https://www.google.com/maps/place/%E3%83%8A%E3%83%93%E3%83%91%E3%83%BC%E3%82%AF+%E4%B8%AD%E8%91%9B%E8%A5%BF%E7%AC%AC%EF%BC%94%EF%BC%93/@35.667624,139.8723594,20z/data=!4m6!3m5!1s0x601887ec5f9643f5:0xe5b9cc5b3719127!8m2!3d35.667624!4d139.872573!16s%2Fg%2F11dxqk3x0y?hl=ja&entry=ttu">
+                ※お車でお越しの方は店舗向かいの有料駐車場をご利用ください。
+            </a>
+        </section>
