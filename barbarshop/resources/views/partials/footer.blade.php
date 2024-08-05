aaf16d13167f4e9e10ee8da8be923a03f4fdfe23
tsukasa <chappieoct4@gmail.com>
2024-08-05 18:28:24 +0900

    一旦削除

--- barbarshop/resources/views/partials/footer.blade.php
+++
@@ -1,3 +0,0 @@
-    <footer class="bg-stone-900 py-4 text-center font-sans">
-        <div>&copy; 2024 BULLS CUT. All rights reserved.</div>
-    </footer>
--- barbarshop/storage/framework/views/766dee102738823663d22a050856817b.php
+++
@@ -1,9 +0,0 @@
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BULLS CUT</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="shortcut icon" href="<?php echo e(asset('BULLSCUT_logomark.ico')); ?>" />
-    <link rel="shortcut icon" href="<?php echo e(asset('BULLSCUT_logomark.png')); ?>" />
-    <script src="https://kit.fontawesome.com/7974e457e6.js" crossorigin="anonymous"></script>
-    <?php echo app('Illuminate\Foundation\Vite')(['resources/css/app.css', 'resources/js/app.js']); ?>
-<?php /**PATH /var/www/html/resources/views/partials/head.blade.php ENDPATH**/ ?>
