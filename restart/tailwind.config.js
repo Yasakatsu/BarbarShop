e5ad8561136a88ac0ec045e094899e36d452145e
tsukasa <chappieoct4@gmail.com>
2024-08-06 17:37:48 +0900

    ページネーション、バリデーション、認証、パスワードリセットのための日本語ファイルの更新

--- restart/tailwind.config.js
+++ restart/tailwind.config.js
@@ -11,40 +11,11 @@ export default {
 
     theme: {
         extend: {
-            // フォントの追加(https://fonts.google.com/specimen/Figtree)
             fontFamily: {
-                sans: ['"Figtree"', ...defaultTheme.fontFamily.sans],
-                fantasy: "fantasy",
-            },
-            colors: {
-                "selected-text": "#A3A3FF",// ここを追加
-                "theme-color": "#5c318c", // ここを追加
-                "accent-color": "#FFC107" // ここを追加
-            },
-            backgroundSize: {
-                'auto': 'auto',
-                'cover': 'cover',
-                'contain': 'contain',
-                '10%': '10%',
-                '20%': '20%',
-                '30%': '30%',
-                '40%': '40%',
-                '50%': '50%',
-                '60%': '60%',
-                '70%': '70%',
-                '80%': '80%',
-                '90%': '90%',
-                '100%': '100%',
-            },
-            spacing: {
-                '21': '5.25rem',
-                '22': '5.5rem',
-                '23': '5.75rem',
-                '61': '15.25rem',
-                '62': '15.5rem',
-                '63': '15.75rem',
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
         },
-        plugins: [forms],
     },
+
+    plugins: [forms],
 };
