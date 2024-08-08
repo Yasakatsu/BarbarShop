405ada0d27153489d0543133ec9ef0ed8b2aff71
tsukasa <chappieoct4@gmail.com>
2024-08-08 22:53:08 +0900

    delete:restartディレクトリ

--- restart/resources/views/sections/reserve.blade.php
+++
@@ -1,7 +0,0 @@
-        <section id="reserve" class="py-20 ">
-            <h2 class="text-3xl font-black mb-4 text-center">Reserve</h2>
-            <div class="text-center">
-                <a href="https://beauty.hotpepper.jp/slnH000520434/"
-                    class=" bg-theme-color  hover:bg-purple-600 duration-300 rounded-md font-bold  py-2 px-4 ">ご予約はこちらから</a>
-            </div>
-        </section>
